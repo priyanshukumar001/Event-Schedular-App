196fab047f364af34d56d5f9c0cd851efc7312a7
pigeon001 <priyanshukashyap1709@gmail.com>
2025-04-06 20:00:43 +0530

    configured typescript and tailwind css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from '../src/App';
+import App from './App';
 import { RouterProvider } from 'react-router-dom';
 
 
