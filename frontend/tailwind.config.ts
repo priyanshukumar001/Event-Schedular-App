77c17b4494662e9d447f85b30553447c7cf4db5c
pigeon001 <priyanshukashyap1709@gmail.com>
2025-06-07 19:19:02 +0530

    updated color themes

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -19,51 +19,52 @@ export default {
 		},
 		extend: {
 			colors: {
-				border: 'hsl(var(--border))',
-				input: 'hsl(var(--input))',
-				ring: 'hsl(var(--ring))',
-				background: 'hsl(var(--background))',
-				foreground: 'hsl(var(--foreground))',
+				border: 'hsl(var(--color-border))',
+				input: 'hsl(var(--color-input))',
+				ring: 'hsl(var(--color-ring))',
+				background: 'hsl(var(--color-background))',
+				foreground: 'hsl(var(--color-foreground))',
 				primary: {
-					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))'
+				  DEFAULT: 'hsl(var(--color-primary))',
+				  foreground: 'hsl(var(--color-primary-foreground))'
 				},
 				secondary: {
-					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))'
+				  DEFAULT: 'hsl(var(--color-secondary))',
+				  foreground: 'hsl(var(--color-secondary-foreground))'
 				},
 				destructive: {
-					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))'
+				  DEFAULT: 'hsl(var(--color-destructive))',
+				  foreground: 'hsl(var(--color-destructive-foreground))'
 				},
 				muted: {
-					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))'
+				  DEFAULT: 'hsl(var(--color-muted))',
+				  foreground: 'hsl(var(--color-muted-foreground))'
 				},
 				accent: {
-					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))'
+				  DEFAULT: 'hsl(var(--color-accent))',
+				  foreground: 'hsl(var(--color-accent-foreground))'
 				},
 				popover: {
-					DEFAULT: 'hsl(var(--popover))',
-					foreground: 'hsl(var(--popover-foreground))'
+				  DEFAULT: 'hsl(var(--color-popover))',
+				  foreground: 'hsl(var(--color-popover-foreground))'
 				},
 				card: {
-					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))'
+				  DEFAULT: 'hsl(var(--color-card))',
+				  foreground: 'hsl(var(--color-card-foreground))'
 				},
 				sidebar: {
-					DEFAULT: 'hsl(var(--sidebar-background))',
-					foreground: 'hsl(var(--sidebar-foreground))',
-					primary: 'hsl(var(--sidebar-primary))',
-					'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-					accent: 'hsl(var(--sidebar-accent))',
-					'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-					border: 'hsl(var(--sidebar-border))',
-					ring: 'hsl(var(--sidebar-ring))'
+				  DEFAULT: 'hsl(var(--color-sidebar-background))',
+				  foreground: 'hsl(var(--color-sidebar-foreground))',
+				  primary: 'hsl(var(--color-sidebar-primary))',
+				  'primary-foreground': 'hsl(var(--color-sidebar-primary-foreground))',
+				  accent: 'hsl(var(--color-sidebar-accent))',
+				  'accent-foreground': 'hsl(var(--color-sidebar-accent-foreground))',
+				  border: 'hsl(var(--color-sidebar-border))',
+				  ring: 'hsl(var(--color-sidebar-ring))'
 				},
-				"blue-brand": "#0073e6",
-			},
+				"blue-brand": 'var(--custom-blue-brand)',
+			  },
+			  
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
